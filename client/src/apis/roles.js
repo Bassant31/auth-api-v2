7ce102dcbcd715d728afc4df6b10bb1d94bc60c4
Bassant31 <bassantnasser31@gmail.com>
2022-03-31 10:10:37 +0200

    add role api

---
+++ client/src/apis/roles.js
@@ -0,0 +1,50 @@
+import instance from "./axios";
+
+
+
+export const createRole =async (name, description)=>{
+
+    const response = await instance({
+        method:'post',
+        url:'/roles',
+        data:{
+            name,
+            description
+        },
+       
+    })
+    return response.data
+
+}
+
+export const getRole = async()=>{
+    const response = await instance({
+        method:'get',
+        url:'/roles'
+    })
+    return response.data
+}
+
+
+export const updateRole =  async (id,name,description)=>{
+    const response = await instance({
+        method:'patch',
+        url:'/roles',
+        data:{
+            id,
+            name,
+            description
+        }
+    })
+    return response.data
+}
+
+export const deleteRole = async (id)=>{
+    const response = await instance({
+        method:'DELETE',
+        url:`/roles/${id}`  
+    })
+    return response.data
+
+
+}
