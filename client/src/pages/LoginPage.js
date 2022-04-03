777658001f5742c20db99a5b6e013aeb59cb87ab
Bassant31 <bassantnasser31@gmail.com>
2022-04-03 12:02:57 +0200

    control show of login when already loggedin

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,22 @@
 import LoginForm from "../components/Authentication/LoginForm";
+import AuthContext from "../store/auth-context"
+import { useHistory } from "react-router";
+import {  useContext } from "react";
 
 const LoginPage = () => {
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
+  const isLoggedIn = authCtx.isLoggedIn
+  if(isLoggedIn){
+    console.log(isLoggedIn)
+    history.replace('/home')
+    return null
+
+  }else{
     return <LoginForm />;
+
+  }
+    
   };
   
   export default LoginPage;
